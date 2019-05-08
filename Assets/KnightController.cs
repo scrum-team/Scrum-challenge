eefcbe3e6316269b34276aff8ee75b574f0832b5
Michał Gorczyca <49516607+pazur1323@users.noreply.github.com>
2019-05-09 00:46:19 +0200

    Add files via upload

---
+++ Assets/KnightController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+    float rot = 0;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if(controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("warunek", 1);
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed;
+                moveDir = transform.TransformDirection(moveDir);
+
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetInteger("warunek", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("warunek", 2);
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed*2;
+                moveDir = transform.TransformDirection(moveDir);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift)&&Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("warunek", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+
+        }
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
