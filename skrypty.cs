fa87a831bf98b4b843c605485f9b8142e2799bc3
Michał Gorczyca <49516607+pazur1323@users.noreply.github.com>
2019-05-15 23:26:33 +0200

    Add files via upload

---
+++ skrypty.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skrypty : MonoBehaviour
+{
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+    float rot = 0;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("war", 1);
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed;
+                moveDir = transform.TransformDirection(moveDir);
+
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetInteger("war", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKey(KeyCode.W))
+            {
+                anim.SetInteger("war", 2);
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed*2;
+                moveDir = transform.TransformDirection(moveDir);
+
+            }
+           
+            if (Input.GetKeyUp(KeyCode.LeftShift) && Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetInteger("war", 0);
+                moveDir = new Vector3(0, 0, 0);
+            }
+        }
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
